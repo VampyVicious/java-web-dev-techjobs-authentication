d0355b7fe6fdb28e54cb85056900882783224080
VampyVicious <vampynguyen@hotmail.com>
2021-11-04 19:21:40 -0500

    edited some stuff

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/dto/LoginFormDTO.java
@@ -4,9 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-/**
- * Created by Chris Bay
- */
+
 public class LoginFormDTO {
 
     @NotNull

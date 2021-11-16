5dd68ceaddc52119c5626f9258ac542179790d5a
VampyVicious <vampynguyen@hotmail.com>
2021-11-15 19:40:37 -0600

    finished studio 19
    finished studio 19

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/JavaWebDevTechJobsAuthentication.java
@@ -0,0 +1,10 @@
+package org.launchcode.javawebdevtechjobsauthentication.models;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebDevTechJobsAuthentication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebDevTechJobsAuthentication.class, args);
+    }
+}
